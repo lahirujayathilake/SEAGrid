3404257cdffaa38e6190ecd2a9e2b466ea338596
Lahiru Jayathilake <lahirujayathilake@gmail.com>
2023-02-13 18:55:02 +0530

    included the parent data product creation

--- seagrid_data_product/views.py
+++ seagrid_data_product/views.py
@@ -15,22 +15,25 @@ def create_seagrid_data_product(request):
     channel = grpc.insecure_channel('localhost:6565')
     stub = DataCatalogAPI_pb2_grpc.DataCatalogAPIServiceStub(channel)
 
+    parent_data_product = DataCatalogAPI_pb2.DataProduct()
+    parent_data_product.name = "parent dp"
+    parent_dp_result = create_data_product(parent_data_product, stub)
+
     data_product = DataCatalogAPI_pb2.DataProduct()
     seagrid_data_product = create_computational_product(request)
 
+    # Common data assignments for Computational, Experimental, and Literature Data Products
     data_product.data_product_id = seagrid_data_product.data_product_id
     data_product.name = seagrid_data_product.name
     data_product.metadata = seagrid_data_product.metadata
+    data_product.parent_data_product_id = parent_dp_result.data_product_id
 
-    # Call Data Catalog API
-    create_request = DataCatalogAPI_pb2.DataProductCreateRequest()
-    create_request.data_product.CopyFrom(data_product)
-
-    create_response = stub.createDataProduct(create_request)
-    print(create_response.data_product)
+    # Call Data Catalog API to create DP
+    result_dp = create_data_product(data_product, stub)
+    print(result_dp)
 
     # Return a response to the client
-    return JsonResponse({'data_product_id': create_response.data_product_id}, status=201)
+    return JsonResponse({'data_product_id': result_dp.data_product_id}, status=201)
 
 
 def create_computational_product(request):
@@ -41,3 +44,10 @@ def create_computational_product(request):
                                               mw=87.0)
     seagrid_data_product.metadata = json.dumps({"mw": seagrid_data_product.mw, "absorb": 834})
     return seagrid_data_product
+
+
+def create_data_product(data_product, stub):
+    create_request = DataCatalogAPI_pb2.DataProductCreateRequest()
+    create_request.data_product.CopyFrom(data_product)
+    create_response = stub.createDataProduct(create_request)
+    return create_response.data_product
